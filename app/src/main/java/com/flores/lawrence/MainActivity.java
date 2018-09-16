d51a1b3f6307e815e6bc6ab728fbd1541517213f
LawrenceFlores <2015085451@ust-ics.mygbiz.com>
2018-09-16 16:38:34 +0800

    Initial commit

---
+++ app/src/main/java/com/flores/lawrence/MainActivity.java
@@ -0,0 +1,13 @@
+package com.flores.lawrence;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint);
+    }
+}
